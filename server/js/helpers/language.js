fb351573c46c3de5f95b794d137d1f085b16b97d
Wouter Vlaeyen <woutvlae@student.arteveldehs.be>
2020-01-18 15:02:00 +0100

    Added more responses

--- server/js/helpers/language.js
+++ server/js/helpers/language.js
@@ -12,6 +12,6 @@ lngDetector.setLanguageType('iso2');
 const detectFromString = (string) => {
   const results = lngDetector.detect(string);
   const available = results.filter((result) => config.availableLanguages.includes(result[0]));
-  return available[0][0];
+  return available.length > 0 ? available[0][0] : 'nl';
 };
 export default detectFromString;
